6dd21a08e8fec56c26b0e9146a40288f26b7a766
Wilson <wilsonmathewone@gmail.com>
2020-06-21 20:52:05 -0600

    Insercion de arbol B, unicamente con enteros. probado y funcional.

--- Proyecto2/src/proyecto2/Proyecto2.java
+++ Proyecto2/src/proyecto2/Proyecto2.java
@@ -16,8 +16,29 @@ public class Proyecto2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Cliente c=new Cliente();
-        c.setVisible(true);
+        //Cliente c=new Cliente();
+        //c.setVisible(true);
+        
+        Vehiculo v = new Vehiculo(1);
+        v.setVisible(true);
+        
+        // Prueba de insercion arbol B
+        /*
+        ArbolB t = new ArbolB(5);
+        t.insertar(10);
+        t.insertar(20);
+        t.insertar(5);
+        t.insertar(6);
+        t.insertar(12);
+        t.insertar(30);
+        t.insertar(7);
+        t.insertar(17);
+        
+        System.out.println("Impresion del arbol construido");
+        t.imprimir();
+        */
+        
+        
         //primer commit
     }
     
