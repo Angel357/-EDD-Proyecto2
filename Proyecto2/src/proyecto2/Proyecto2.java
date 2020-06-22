fc4bf717691669c113b43051d538fa76b7eae754
Angel357 <30792156+Angel357@users.noreply.github.com>
2020-06-21 22:01:39 -0600

    empezando prueba de tabla hash, 201603079, Angel Castellanos

--- Proyecto2/src/proyecto2/Proyecto2.java
+++ Proyecto2/src/proyecto2/Proyecto2.java
@@ -5,6 +5,7 @@
  */
 package proyecto2;
 
+import java.math.BigInteger;
 /**
  *
  * @author luisa
@@ -15,34 +16,14 @@ public class Proyecto2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        //Cliente c=new Cliente();
-        //c.setVisible(true);
-        
-        Vehiculo v = new Vehiculo(1);
-        v.setVisible(true);
-        
-        
-        String placa1 = "";
-        String placa2 = "";
-        
-        if(placa1.compareToIgnoreCase(placa2)==0)
-            System.out.println();
-        // Prueba de insercion arbol B
-        /*
-        ArbolB t = new ArbolB(5);
-        t.insertar(10);
-        t.insertar(20);
-        t.insertar(5);
-        t.insertar(6);
-        t.insertar(12);
-        t.insertar(30);
-        t.insertar(7);
-        t.insertar(17);
-        
-        System.out.println("Impresion del arbol construido");
-        t.imprimir();
-        */
+      
+//        TablaHash t=new TablaHash(37);
+//        MenuCliente m=new MenuCliente();
+//        m.setHash(t);
+//        m.setVisible(true);
+        BigInteger dpi=new BigInteger("3002875340101");
+        BigInteger mod=new BigInteger("37");
+        System.out.println(dpi.mod(mod));
         
         
         //primer commit
